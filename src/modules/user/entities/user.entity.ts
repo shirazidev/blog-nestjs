05f89ba97cba3fdf6f7f3d17e54fca54f994c225
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-10 00:33:19 +0330

    feat: implement UserEntity with properties and timestamps for user management

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntityNames } from "src/common/enums/entity.enum";
+import { Column, CreateDateColumn, Entity, UpdateDateColumn } from "typeorm";
+
+@Entity(EntityNames.User)
+export class UserEntity extends BaseEntity {
+    @Column({unique: true})
+    username: string;
+    @Column({unique: true, nullable: true})
+    phone: string;
+    @Column({unique: true, nullable: true})
+    email: string;
+    @Column({nullable: true})
+    password: string;
+    @CreateDateColumn()
+    created_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
+}
