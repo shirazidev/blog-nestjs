cedad0c9df779bc25c7bda1f5e565fd38366afba
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-09 23:21:26 +0330

    fix: destructure PORT from process.env for improved readability

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { SwaggerConfigInit } from './config/swagger.config';
-const port = process.env.PORT ?? 3000
+const {PORT} = process.env ?? 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   SwaggerConfigInit(app);
-  await app.listen(port, () => {
-    console.log(`app running on port ${port}: http://localhost:${port}`);
+  await app.listen(PORT, () => {
+    console.log(`app running on port ${PORT}: http://localhost:${PORT}`);
   });
 }
 bootstrap();
