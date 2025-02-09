b155065a820f38aac0fddc724f1ef3f04327ddc4
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-09 04:00:11 +0330

    edit and some refactor on file path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { AppModule } from './modules/app/app.module';
+const port = process.env.PORT ?? 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, () => {
+    console.log(`app running on port ${port}: http://localhost:${port}`);
+  });
 }
 bootstrap();
