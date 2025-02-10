5ce43f449e8b4ba2a668f1a451ddd6f9f4a359c5
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-10 15:11:50 +0330

    feat: enhance AuthService with user existence checks and validation methods

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../user/entities/user.entity';
 import { Repository } from 'typeorm';
+import { AuthTypeEnum } from './enums/type.enum';
+import { AuthMethod } from './enums/method.enum';
+import { isEmail, isMobilePhone } from 'class-validator';
 
 @Injectable()
 export class AuthService {
@@ -10,7 +17,37 @@ export class AuthService {
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
   ) {}
-  userExistence(authDto: AuthDto) {
-    return authDto
+  async userExistence(authDto: AuthDto) {
+    const { username, method, type } = authDto;
+    switch (type) {
+      case AuthTypeEnum.Login:
+        return this.login(username, method);
+      case AuthTypeEnum.Register:
+        return this.register(username, method);
+
+      default:
+        throw new UnauthorizedException('Invalid auth type');
+    }
+  }
+  login(username: string, method: AuthMethod) {
+    this.usernameValidator(username, method);
+  }
+  register(username: string, method: AuthMethod) {
+    this.usernameValidator(username, method);
+  }
+  usernameValidator(username: string, method: AuthMethod) {
+    switch (method) {
+      case AuthMethod.Email:
+        if (isEmail(username)) return username;
+        throw new BadRequestException('Invalid email');
+      case AuthMethod.Phone:
+        if (isMobilePhone(username, 'fa-IR')) return username;
+        throw new BadRequestException('Invalid phone number');
+      case AuthMethod.Username:
+        return username;
+
+      default:
+        throw new UnauthorizedException('Invalid username data');
+    }
   }
 }
