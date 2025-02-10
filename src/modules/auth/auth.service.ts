4364f5119ac9021e1b2452176bef7eeae1d7cff8
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-10 15:38:00 +0330

    feat: enhance AuthService with user existence checks and improve login/register methods

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,12 +10,16 @@ import { Repository } from 'typeorm';
 import { AuthTypeEnum } from './enums/type.enum';
 import { AuthMethod } from './enums/method.enum';
 import { isEmail, isMobilePhone } from 'class-validator';
+import { ProfileEntity } from '../user/entities/profile.entity';
+import { AuthMessage, BadRequestMessage } from 'src/common/enums/message.enum';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
+    @InjectRepository(ProfileEntity)
+    private profileRepository: Repository<ProfileEntity>,
   ) {}
   async userExistence(authDto: AuthDto) {
     const { username, method, type } = authDto;
@@ -29,11 +33,30 @@ export class AuthService {
         throw new UnauthorizedException('Invalid auth type');
     }
   }
-  login(username: string, method: AuthMethod) {
-    this.usernameValidator(username, method);
+  async login(username: string, method: AuthMethod) {
+    const validUsername =this.usernameValidator(username, method);
+    let user: UserEntity = await this.checkExistUser(method, validUsername);
+    if(!user)throw new UnauthorizedException(AuthMessage.UserNotFound);
   }
-  register(username: string, method: AuthMethod) {
-    this.usernameValidator(username, method);
+  async register(username: string, method: AuthMethod) {
+    const validUsername =this.usernameValidator(username, method);
+    let user: UserEntity = await this.checkExistUser(method, validUsername);
+    if(user)throw new UnauthorizedException(AuthMessage.InvalidAuthType);
+  }
+  async checkOtp(){}
+  async checkExistUser(method: AuthMethod, username: string) {
+    let user;
+    if(method === AuthMethod.Username){
+      user = await this.userRepository.findOneBy({username: username});
+    }else if(method === AuthMethod.Phone){
+      user = await this.userRepository.findOneBy({phone: username});
+    }else if(method === AuthMethod.Email){
+      user = await this.userRepository.findOneBy({email: username});
+    }else{
+      throw new UnauthorizedException(BadRequestMessage.InvalidLoginData);
+    }
+    if(!user)throw new UnauthorizedException(AuthMessage.UserNotFound);
+    return user;
   }
   usernameValidator(username: string, method: AuthMethod) {
     switch (method) {
