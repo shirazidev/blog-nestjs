09b07d0b314636a432ae114a305a4e0aa5f2ff49
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-10 15:37:49 +0330

    feat: add message enums for authentication and validation responses

---
+++ src/common/enums/message.enum.ts
@@ -0,0 +1,15 @@
+export enum BadRequestMessage {
+    InvalidLoginData="اطلاعات ارسال شده برای احراز هویت نامعتبر است",
+    InvalidRegisterData="اطلاعات ارسال شده برای ثبت نام نامعتبر است",
+    }
+export enum AuthMessage {
+    InvalidAuthType = 'حساب کاربری با این مشخصات قبلا ثبت شده است',
+    InvalidLogin = 'Invalid login',
+    UserNotFound = 'User not found',
+    }
+export enum NotFoundMessage {
+
+}
+export enum ValidationMessage {
+
+}
