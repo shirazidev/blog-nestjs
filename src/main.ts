d9601094f376d03e46a73061ff051e1dabda9842
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-11 04:26:46 +0330

    feat: integrate cookie-parser middleware with COOKIE_SECRET for enhanced cookie handling

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { SwaggerConfigInit } from './config/swagger.config';
-const {PORT} = process.env
+import * as cookieParser from 'cookie-parser';
+const { PORT, COOKIE_SECRET } = process.env;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser(COOKIE_SECRET));
   SwaggerConfigInit(app);
   await app.listen(PORT, () => {
     console.log(`app running on port ${PORT}: http://localhost:${PORT}`);
