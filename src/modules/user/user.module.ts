3ab5851f90790855524c392f262158860cd00ce0
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-11 03:38:10 +0330

    feat: integrate OtpEntity into UserModule for OTP management

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,10 +4,12 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { ProfileEntity } from './entities/profile.entity';
+import { OtpEntity } from './entities/otp.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, ProfileEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, ProfileEntity, OtpEntity])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService, TypeOrmModule],
 })
 export class UserModule {}
