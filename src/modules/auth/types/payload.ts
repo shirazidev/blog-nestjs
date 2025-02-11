775845c307bf4d848a7e0e285c3f2a0618a25397
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-11 15:02:20 +0330

    feat: add JwtPayload and JwtVerify types for enhanced JWT handling

--- src/modules/auth/types/payload.ts
+++ src/modules/auth/types/payload.ts
@@ -1,3 +1,11 @@
 export type CookiePayload = {
     userId: number
 }
+export type JwtPayload = {
+    userId: number,
+    username: string
+}
+export type JwtVerify = {
+    accessToken: string,
+    refreshToken: string,
+}
