7d2e5c23ae9e9c766adc05c2d981a591319e9549
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-11 04:27:44 +0330

    feat: add JwtService and TokenService to AuthModule providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { UserEntity } from '../user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfileEntity } from '../user/entities/profile.entity';
 import { UserModule } from '../user/user.module';
+import { TokenService } from './tokens.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [UserModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService, TokenService],
 })
 export class AuthModule {}
