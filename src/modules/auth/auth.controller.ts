d42bfb9dd2a18fca26272632bffd446c01713643
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-11 04:28:13 +0330

    feat: update userExistence method to support response handling with Express

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { AuthDto } from './dto/auth.dto';
 import { SwaggerConsumesEnum } from 'src/common/enums/swagger-consumes.enum';
+import { Response } from 'express';
+import { cookieKeys } from 'src/common/enums/cookie.enum';
 
 
 @Controller('auth')
@@ -11,7 +13,9 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('/user-existence')
   @ApiConsumes(SwaggerConsumesEnum.FORM, SwaggerConsumesEnum.JSON)
-  userExistence(@Body() authDto: AuthDto) {
-    return this.authService.userExistence(authDto);
+  async userExistence(@Body() authDto: AuthDto, @Res() res: Response) {
+    const result = await this.authService.userExistence(authDto, res);
+    // for production projects we use secure cookie names
+
   }
 }
