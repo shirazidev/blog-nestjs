7f640934f84966a16b95e39b4d26520d6f5debd7
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-12 04:16:06 +0330

    feat: add Pagination decorator for Swagger API documentation

---
+++ src/common/decorators/pagination.decorator.ts
@@ -0,0 +1,15 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
+
+export function Pagination() {
+  return applyDecorators(
+    ApiQuery({
+      name: 'page',
+      example: 1,
+    }),
+    ApiQuery({
+      name: 'limit',
+      example: 10,
+    }),
+  );
+}
