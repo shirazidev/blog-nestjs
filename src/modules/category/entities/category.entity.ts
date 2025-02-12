a39d94275f49337326a7455e5c34da564840431b
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-12 04:03:03 +0330

    feat: extend CategoryEntity with BaseEntity and add title and priority fields

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,7 +1,12 @@
-import { EntityNames } from "src/common/enums/entity.enum";
-import { Entity } from "typeorm";
+import { BaseEntity } from 'src/common/abstracts/base.entity';
+import { EntityNames } from 'src/common/enums/entity.enum';
+import { Column, Entity } from 'typeorm';
 
 @Entity(EntityNames.Category)
-export class CategoryEntity {
+export class CategoryEntity extends BaseEntity {
+    @Column()
+    title: string
+    @Column({nullable: true})
+    priority: number
     
 }
