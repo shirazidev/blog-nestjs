a32d0dcb12ff062ba30920060164912e38e401b4
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-12 04:03:09 +0330

    feat: enhance CreateCategoryDto with title and priority fields

--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,1 +1,9 @@
-export class CreateCategoryDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsString, Length } from 'class-validator';
+
+export class CreateCategoryDto {
+  @ApiProperty()
+  title: string;
+  @ApiPropertyOptional()
+  priority: number;
+}
