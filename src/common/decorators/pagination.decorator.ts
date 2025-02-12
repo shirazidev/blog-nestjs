a9be7a6d1686aaa4b9da4f3c342f2ef33f27fa8a
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-12 04:22:48 +0330

    feat: update Pagination decorator to mark page and limit as optional

--- src/common/decorators/pagination.decorator.ts
+++ src/common/decorators/pagination.decorator.ts
@@ -6,10 +6,14 @@ export function Pagination() {
     ApiQuery({
       name: 'page',
       example: 1,
+      required: false,
+      type: "integer",
     }),
     ApiQuery({
       name: 'limit',
       example: 10,
+      required: false,
+      type: "integer",
     }),
   );
 }
