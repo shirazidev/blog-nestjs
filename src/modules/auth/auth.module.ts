b277d0460f9a0a0545efb83bb723e6f9f1ee5c90
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-12 04:02:34 +0330

    feat: add AuthGuard to AuthModule providers and exports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { TokenService } from './tokens.service';
 import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [UserModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtService, TokenService],
+  providers: [AuthService, JwtService, TokenService, AuthGuard],
+  exports: [AuthGuard, AuthService, JwtService, TokenService]
 })
 export class AuthModule {}
