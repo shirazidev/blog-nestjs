643d82078c1b596ab370bcd4fcf99dea8f307e71
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-14 01:32:05 +0330

    feat: enhance changeProfile method to support multiple file uploads

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -9,22 +9,49 @@ import {
   Delete,
   Put,
   UseGuards,
+  UseInterceptors,
+  UploadedFiles,
+  ParseFilePipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { SwaggerConsumesEnum } from 'src/common/enums/swagger-consumes.enum';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import {
+  multerDestination,
+  multerFilename,
+} from 'src/common/utils/multer.util';
 
 @Controller('user')
 @ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Put('/change-profile')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'image_profile', maxCount: 1 },
+        { name: 'image_bg', maxCount: 1 },
+      ],
+      {
+        storage: diskStorage({
+          destination: multerDestination('user-profile'),
+          filename: multerFilename,
+        }),
+      },
+    ),
+  )
   @UseGuards(AuthGuard)
   @ApiBearerAuth('Authorization')
   @ApiConsumes(SwaggerConsumesEnum.MULTIPART)
-  async changeProfile(@Body() profileDto: ProfileDto) {
-    return this.userService.changeProfile(profileDto);
+  async changeProfile(
+    @UploadedFiles(new ParseFilePipe({ fileIsRequired: false, validators: [] }))
+    files: any,
+    @Body() profileDto: ProfileDto,
+  ) {
+    return this.userService.changeProfile(files, profileDto);
   }
 }
