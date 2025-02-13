9ff4193f4cea34efb85d3cb3924428ff5fe74ad5
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-14 00:51:45 +0330

    feat: add change profile endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ProfileDto } from './dto/user.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { SwaggerConsumesEnum } from 'src/common/enums/swagger-consumes.enum';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 @Controller('user')
-@ApiTags("User")
+@ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
+  @Put('/change-profile')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('Authorization')
+  @ApiConsumes(SwaggerConsumesEnum.MULTIPART)
+  async changeProfile(@Body() profileDto: ProfileDto) {
+    return this.userService.changeProfile(profileDto);
+  }
 }
