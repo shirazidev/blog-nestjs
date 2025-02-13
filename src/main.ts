932520dc65e0122ae80b9335d03bd9e264482d49
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-14 02:40:17 +0330

    feat: add global validation pipe to enhance request validation

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './modules/app/app.module';
 import { SwaggerConfigInit } from './config/swagger.config';
 import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 const { PORT, COOKIE_SECRET } = process.env;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser(COOKIE_SECRET));
   app.useStaticAssets('public');
+  app.useGlobalPipes(new ValidationPipe());
   SwaggerConfigInit(app);
   await app.listen(PORT, () => {
     console.log(`app running on port ${PORT}: http://localhost:${PORT}`);
