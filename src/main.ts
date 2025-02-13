bc3dccd35dabe4e35995a393a3b982b2371863d8
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-14 02:22:51 +0330

    feat: enhance application setup to use NestExpressApplication and serve static assets

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { SwaggerConfigInit } from './config/swagger.config';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 const { PORT, COOKIE_SECRET } = process.env;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser(COOKIE_SECRET));
+  app.useStaticAssets('public');
   SwaggerConfigInit(app);
   await app.listen(PORT, () => {
     console.log(`app running on port ${PORT}: http://localhost:${PORT}`);
