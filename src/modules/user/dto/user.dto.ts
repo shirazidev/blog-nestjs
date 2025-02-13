16431ad41a9dc5364332201292bc11b477b3a534
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-14 02:40:22 +0330

    feat: update ProfileDto to make fields optional for improved flexibility

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -1,25 +1,33 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, isEnum, Length } from 'class-validator';
+import { IsEnum, isEnum, IsOptional, Length } from 'class-validator';
 import { Gender } from '../enums/gender.enum';
 
 export class ProfileDto {
   @ApiPropertyOptional()
+  @IsOptional()
   @Length(3, 100)
   nick_name: string;
   @ApiPropertyOptional()
+  @IsOptional()
   @Length(10, 200)
   bio: string;
   @ApiPropertyOptional({format: "binary"})
+  @IsOptional()
   image_profile: string;
   @ApiPropertyOptional({format: "binary"})
+  @IsOptional()
   image_bg: string;
   @ApiPropertyOptional({enum: Gender})
+  @IsOptional()
   @IsEnum(Gender)
   gender: string;
   @ApiPropertyOptional({example: "2025-02-13T15:18:49.740Z"})
+  @IsOptional()
   birth_date: Date;
   @ApiPropertyOptional()
+  @IsOptional()
   linkedin: string;
   @ApiPropertyOptional()
+  @IsOptional()
   twitter: string;
 }
