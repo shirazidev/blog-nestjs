8cba9d6fc283971b55849789d1d25bc3a8195c25
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-16 22:45:24 +0330

    feat: create BlogBookmarksEntity to represent bookmarks for blog posts with user associations

---
+++ src/modules/blog/entities/bookmark.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity } from 'src/common/abstracts/base.entity';
+import { EntityNames } from 'src/common/enums/entity.enum';
+import { UserEntity } from 'src/modules/user/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BlogEntity } from './blog.entity';
+
+@Entity(EntityNames.BlogBookmarks)
+export class BlogBookmarksEntity extends BaseEntity {
+  @Column()
+  blogId: number;
+  @Column()
+  userId: number;
+  @ManyToOne(() => UserEntity, (user) => user.blog_bookmarks, {
+    onDelete: 'CASCADE',
+  })
+  user: UserEntity;
+  @ManyToOne(() => BlogEntity, (blog) => blog.bookmarks, { onDelete: 'CASCADE' })
+  blog: BlogEntity;
+}
