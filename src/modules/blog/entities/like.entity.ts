38f078c28d650d29c4b69c5c3bd3c28cf3947380
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-16 22:45:18 +0330

    feat: create BlogLikeEntity to represent likes on blog posts with user associations

---
+++ src/modules/blog/entities/like.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity } from 'src/common/abstracts/base.entity';
+import { EntityNames } from 'src/common/enums/entity.enum';
+import { UserEntity } from 'src/modules/user/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BlogEntity } from './blog.entity';
+
+@Entity(EntityNames.BlogLikes)
+export class BlogLikeEntity extends BaseEntity {
+  @Column()
+  blogId: number;
+  @Column()
+  userId: number;
+  @ManyToOne(() => UserEntity, (user) => user.blog_likes, {
+    onDelete: 'CASCADE',
+  })
+  user: UserEntity;
+  @ManyToOne(() => BlogEntity, (blog) => blog.likes, { onDelete: 'CASCADE' })
+  blog: BlogEntity;
+}
