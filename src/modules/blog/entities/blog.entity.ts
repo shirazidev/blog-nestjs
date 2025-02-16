534a57c5ec5897299052575f61895868d66d48e1
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-16 22:45:07 +0330

    feat: create BlogEntity to represent blog posts with related properties and relationships

---
+++ src/modules/blog/entities/blog.entity.ts
@@ -0,0 +1,42 @@
+import { BaseEntity } from 'src/common/abstracts/base.entity';
+import { EntityNames } from 'src/common/enums/entity.enum';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { BlogStatus } from '../enums/status.enum';
+import { UserEntity } from 'src/modules/user/entities/user.entity';
+import { BlogLikeEntity } from './like.entity';
+import { BlogBookmarksEntity } from './bookmark.entity';
+import { BlogCommentsEntity } from './comment.entity';
+
+@Entity(EntityNames.Blog)
+export class BlogEntity extends BaseEntity {
+  @Column()
+  title: string;
+  @Column()
+  short_desc: string;
+  @Column()
+  content: string;
+  @Column({ nullable: true })
+  image: string;
+  @Column()
+  authorId: number;
+  @ManyToOne(() => UserEntity, (user) => user.blogs, { onDelete: 'CASCADE' })
+  author: UserEntity;
+  @OneToMany(() => BlogLikeEntity, (like) => like.blog)
+  likes: BlogLikeEntity[];
+  @OneToMany(() => BlogBookmarksEntity, (bookmark) => bookmark.blog)
+  bookmarks: BlogBookmarksEntity[];
+  @OneToMany(() => BlogCommentsEntity, (comment) => comment.blog)
+  comments: BlogCommentsEntity[];
+  @Column({ default: BlogStatus.Draft })
+  status: string;
+  @CreateDateColumn()
+  created_at: Date;
+  @CreateDateColumn()
+  updated_at: Date;
+}
