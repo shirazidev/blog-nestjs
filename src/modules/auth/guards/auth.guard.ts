8633f41503216eb9663c6d49a3e92128e79f2e15
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-18 03:22:47 +0330

    feat: enhance AuthGuard to support skipping authentication for specific routes

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -9,11 +9,21 @@ import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { AuthMessage } from 'src/common/enums/message.enum';
 import { AuthService } from '../auth.service';
+import { Reflector } from '@nestjs/core';
+import { SKIP_AUTH } from '../../../common/decorators/skip-auth.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private reflector: Reflector,
+  ) {}
   async canActivate(context: ExecutionContext) {
+    const isSkippedAuthorization = this.reflector.get<boolean>(
+      SKIP_AUTH,
+      context.getHandler(),
+    );
+    if (isSkippedAuthorization) return true;
     const httpContext = context.switchToHttp();
     const request: Request = httpContext.getRequest<Request>();
     const token = this.extractToken(request);
