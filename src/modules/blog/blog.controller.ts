f4298b14c5d23a6505bde759313370ed690f30e5
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-18 03:23:15 +0330

    feat: add find method to retrieve paginated blog list without authentication

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,26 +1,32 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dtos/blog.dto';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { SwaggerConsumesEnum } from 'src/common/enums/swagger-consumes.enum';
+import { PaginationDto } from '../../common/dtos/pagination.dto';
+import { SkipAuth } from '../../common/decorators/skip-auth.decorator';
 
 @Controller('blog')
+@UseGuards(AuthGuard)
+@ApiBearerAuth('Authorization')
 @ApiTags('Blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
   @Post('/')
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth('Authorization')
   @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
   async createBlog(@Body() createBlogDto: CreateBlogDto) {
     return await this.blogService.createBlog(createBlogDto);
   }
   @Get('/user')
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth('Authorization')
   @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
   async getUserBlogs() {
     return await this.blogService.getUserBlogs();
+  }
+  @Get('/')
+  @SkipAuth()
+  @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
+  async find(@Query() paginationDto: PaginationDto) {
+    return await this.blogService.blogsList(paginationDto);
   }
 }
