1eb66bfd681c32eed30843b344452337d1187e26
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-18 02:49:16 +0330

    feat: enhance createBlog method to include user validation and unique slug generation

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -1,20 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, Scope } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BlogEntity } from './entities/blog.entity';
 import { Repository } from 'typeorm';
 import { CreateBlogDto } from './dtos/blog.dto';
-import { createSlug } from 'src/common/utils/functions.util';
+import { createSlug, randomId } from 'src/common/utils/functions.util';
+import { Request } from 'express';
+import {
+  BadRequestMessage,
+  PublicMessage,
+} from '../../common/enums/message.enum';
+import { REQUEST } from '@nestjs/core';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class BlogService {
-    constructor(
-        @InjectRepository(BlogEntity)
-        private blogRepository: Repository<BlogEntity>,
-    ){}
-    async createBlog(createBlogDto: CreateBlogDto) {
-        let {title, slug} = createBlogDto;
-        let slugData = slug ?? title
-        createBlogDto.slug = createSlug(slugData)
-        return createBlogDto
-    }
+  constructor(
+    @InjectRepository(BlogEntity)
+    private blogRepository: Repository<BlogEntity>,
+    @Inject(REQUEST) private request: Request,
+  ) {}
+  async createBlog(createBlogDto: CreateBlogDto) {
+    let { title, slug, content, time_for_study, short_desc, image } =
+      createBlogDto;
+    let user = this.request?.user;
+    if (!user) throw new BadRequestException(BadRequestMessage.SomeThingWrong);
+    slug = createSlug(slug?.trim() || title);
+    const isExist = await this.checkExistBySlug(slug);
+    if (isExist) slug += `-${randomId()}`;
+    let blog = this.blogRepository.create({
+      title,
+      slug,
+      content,
+      time_for_study,
+      short_desc,
+      image,
+      authorId: user.id,
+    });
+    await this.blogRepository.save(blog);
+    return { message: PublicMessage.Created };
+  }
+  async checkExistBySlug(slug: string) {
+    const blog = await this.blogRepository.findOneBy({ slug });
+    return !!blog;
+  }
 }
