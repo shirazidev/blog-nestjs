95e954e20ddd417bffba37ca309e5419cf082dab
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-18 02:49:06 +0330

    feat: update CreateBlogDto to include IsNumberString validation for time_for_study

--- src/modules/blog/dtos/blog.dto.ts
+++ src/modules/blog/dtos/blog.dto.ts
@@ -1,25 +1,25 @@
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { IsNotEmpty, Length } from "class-validator";
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsNumberString, Length } from 'class-validator';
 
 export class CreateBlogDto {
-    @ApiProperty()
-    @IsNotEmpty()
-    @Length(10, 150)
-    title: string;
-    @ApiPropertyOptional()
-    slug: string;
-    @ApiProperty()
-    @IsNotEmpty()
-    time_for_study: string;
-    @ApiProperty()
-    @IsNotEmpty()
-    @Length(10, 300)
-    short_desc: string;
-    @ApiProperty()
-    @IsNotEmpty()
-    @Length(100)
-    content: string;
-    @ApiPropertyOptional({format: "binary"})
-    image: string;
-
+  @ApiProperty()
+  @IsNotEmpty()
+  @Length(10, 150)
+  title: string;
+  @ApiPropertyOptional()
+  slug: string;
+  @ApiProperty()
+  @IsNumberString()
+  @IsNotEmpty()
+  time_for_study: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  @Length(10, 300)
+  short_desc: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  @Length(100)
+  content: string;
+  @ApiPropertyOptional()
+  image: string;
 }
