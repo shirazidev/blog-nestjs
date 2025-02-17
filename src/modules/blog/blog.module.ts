e790bd341af940737aa60a5079e6c4bba86ffb19
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-17 04:26:23 +0330

    feat: add TypeOrmModule imports for blog-related entities and AuthModule

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogEntity } from './entities/blog.entity';
+import { BlogLikeEntity } from './entities/like.entity';
+import { BlogCommentsEntity } from './entities/comment.entity';
+import { BlogBookmarksEntity } from './entities/bookmark.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([
+      BlogEntity,
+      BlogLikeEntity,
+      BlogCommentsEntity,
+      BlogBookmarksEntity,
+    ]),
+  ],
   controllers: [BlogController],
   providers: [BlogService],
 })
