ce7b07f376c52b8825e1a0ea07b57577ed34cec9
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-18 02:48:46 +0330

    feat: add randomId utility function and update createSlug for consistent formatting

--- src/common/utils/functions.util.ts
+++ src/common/utils/functions.util.ts
@@ -1,3 +1,6 @@
+import { randomInt } from 'crypto';
+
 export function createSlug(str: string) {
-    return str.replace(/[،ًًًٌٍُِ\.\+\-_)(*&^%$#@!~'";:?><«»`ء]+/g, '').replace(/[\s]+/g, '-')
+  return `${str.replace(/[،ًًًٌٍُِ\.\+\-_)(*&^%$#@!~'";:?><«»`ء]+/g, '').replace(/[\s]+/g, '-')}`;
 }
+export const randomId = () => Math.random().toString(36).substring(2);
