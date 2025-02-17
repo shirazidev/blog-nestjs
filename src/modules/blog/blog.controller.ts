13f0795a9a278f9b77049f9332ee62da27a8e733
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-18 02:49:25 +0330

    feat: add ApiTags decorator to BlogController for improved Swagger documentation

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -2,10 +2,11 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dtos/blog.dto';
 import { AuthGuard } from '../auth/guards/auth.guard';
-import { ApiBearerAuth, ApiConsumes } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { SwaggerConsumesEnum } from 'src/common/enums/swagger-consumes.enum';
 
 @Controller('blog')
+@ApiTags('Blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
   @Post('/')
