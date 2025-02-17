b153c0e61ed390a5cb7865702ddf142ae93f49cf
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-17 04:52:55 +0330

    feat: implement createBlog method in BlogService for blog creation with slug generation

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BlogEntity } from './entities/blog.entity';
+import { Repository } from 'typeorm';
+import { CreateBlogDto } from './dtos/blog.dto';
+import { createSlug } from 'src/common/utils/functions.util';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+    constructor(
+        @InjectRepository(BlogEntity)
+        private blogRepository: Repository<BlogEntity>,
+    ){}
+    async createBlog(createBlogDto: CreateBlogDto) {
+        let {title, slug} = createBlogDto;
+        let slugData = slug ?? title
+        createBlogDto.slug = createSlug(slugData)
+        return createBlogDto
+    }
+}
