75a049e942df40f7f01ca446c4bfa80d9369f13d
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-18 03:41:39 +0330

    feat: add one-to-many relation between CategoryEntity and BlogCategoryEntity

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,12 +1,14 @@
 import { BaseEntity } from 'src/common/abstracts/base.entity';
 import { EntityNames } from 'src/common/enums/entity.enum';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { BlogCategoryEntity } from '../../blog/entities/blog-category.entity';
 
 @Entity(EntityNames.Category)
 export class CategoryEntity extends BaseEntity {
-    @Column()
-    title: string
-    @Column({nullable: true})
-    priority: number
-    
+  @Column()
+  title: string;
+  @Column({ nullable: true })
+  priority: number;
+  @OneToMany(() => BlogCategoryEntity, (blog) => blog.category)
+  blog_categories: BlogCategoryEntity[];
 }
