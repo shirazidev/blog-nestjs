82e1179908c49534a09950b49ee0a598960ca7c6
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-23 11:45:19 +0330

    feat: add FilterBlog decorator to support category filtering in API queries

---
+++ src/common/decorators/filter.decorator.ts
@@ -0,0 +1,13 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
+
+export function FilterBlog() {
+  return applyDecorators(
+    ApiQuery({
+      name: 'category',
+      example: 'string',
+      required: false,
+      type: 'string',
+    }),
+  );
+}
