e5dc5f1639655f2ff1fc7b9f4472dbfd9044df43
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-27 21:13:48 +0330

    feat: add CommentsController for handling comment creation

---
+++ src/modules/blog/controllers/comments.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../../auth/guards/auth.guard';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { BlogService } from '../services/blog.service';
+import { CommentsService } from '../services/comments.service';
+import { SwaggerConsumesEnum } from '../../../common/enums/swagger-consumes.enum';
+import { CreateBlogDto } from '../dtos/blog.dto';
+import { CommentDto } from '../dtos/comment.dto';
+
+@Controller('blog/comment/')
+@UseGuards(AuthGuard)
+@ApiBearerAuth('Authorization')
+@ApiTags('Blog-Comments')
+export class CommentsController {
+  constructor(private readonly commentService: CommentsService) {}
+  @Post('/')
+  @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
+  async createBlog(@Body() createCommentDto: CommentDto) {
+    return await this.commentService.createComment(createCommentDto);
+  }
+}
