9b13054ac5a4da7664997964f8d48e65b5e849da
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-02-27 21:18:13 +0330

    refactor: remove unused imports in CommentDto and CommentsController

--- src/modules/blog/controllers/comments.controller.ts
+++ src/modules/blog/controllers/comments.controller.ts
@@ -1,10 +1,8 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../../auth/guards/auth.guard';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
-import { BlogService } from '../services/blog.service';
 import { CommentsService } from '../services/comments.service';
 import { SwaggerConsumesEnum } from '../../../common/enums/swagger-consumes.enum';
-import { CreateBlogDto } from '../dtos/blog.dto';
 import { CommentDto } from '../dtos/comment.dto';
 
 @Controller('blog/comment/')
