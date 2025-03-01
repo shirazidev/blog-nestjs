acd70ed464a71e98c5c13e9695662c33b45d1ac7
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-01 20:57:30 +0330

    feat: add endpoint to retrieve comments with pagination and filtering

--- src/modules/blog/controllers/comments.controller.ts
+++ src/modules/blog/controllers/comments.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '../../auth/guards/auth.guard';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { CommentsService } from '../services/comments.service';
 import { SwaggerConsumesEnum } from '../../../common/enums/swagger-consumes.enum';
 import { CommentDto } from '../dtos/comment.dto';
+import { SkipAuth } from '../../../common/decorators/skip-auth.decorator';
+import { FilterBlog } from '../../../common/decorators/filter.decorator';
+import { PaginationDto } from '../../../common/dtos/pagination.dto';
+import { CommentQueryDto, FilterBlogDto } from '../dtos/blog.dto';
+import { Pagination } from '../../../common/decorators/pagination.decorator';
 
 @Controller('blog/comment/')
 @UseGuards(AuthGuard)
@@ -11,6 +24,16 @@ import { CommentDto } from '../dtos/comment.dto';
 @ApiTags('Blog-Comments')
 export class CommentsController {
   constructor(private readonly commentService: CommentsService) {}
+  @Get('/')
+  @SkipAuth()
+  @Pagination()
+  @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
+  async findComments(
+    @Query() idDto: CommentQueryDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    return await this.commentService.findPostComments(idDto, paginationDto);
+  }
   @Post('/')
   @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
   async createBlog(@Body() createCommentDto: CommentDto) {
