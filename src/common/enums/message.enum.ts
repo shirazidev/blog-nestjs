8563403c7caa8bc55cb932b4ebd3da59094d856f
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-06 12:25:11 +0330

    feat: add NotFoundImage message to NotFoundMessage enum

--- src/common/enums/message.enum.ts
+++ src/common/enums/message.enum.ts
@@ -27,6 +27,7 @@ export enum AuthMessage {
 export enum NotFoundMessage {
   NotFoundCategory = 'دسته بندی یافت نشد.',
   NotFoundBlog = 'مقاله مورد نظر یافت نشد.',
+  NotFoundImage = 'تصویر یافت نشد.',
 }
 export enum ValidationMessage {
   ImageFormatIncorrect = 'فرمت تصویر باید یکی از فرمت های زیر باشد: png, jpg, jpeg, gif, svg, webp, bmp, tiff, ico, jpe',
