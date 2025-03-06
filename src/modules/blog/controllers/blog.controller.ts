c79b114621722fa4458b0101d9e35d94d613d8b2
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-06 11:51:46 +0330

    refactor: remove unused imports from blog.controller.ts

--- src/modules/blog/controllers/blog.controller.ts
+++ src/modules/blog/controllers/blog.controller.ts
@@ -8,12 +8,10 @@ import {
   Post,
   Put,
   Query,
-  UseGuards,
 } from '@nestjs/common';
 import { BlogService } from '../services/blog.service';
 import { CreateBlogDto, FilterBlogDto, UpdateBlogDto } from '../dtos/blog.dto';
-import { AuthGuard } from '../../auth/guards/auth.guard';
-import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { SwaggerConsumesEnum } from 'src/common/enums/swagger-consumes.enum';
 import { PaginationDto } from '../../../common/dtos/pagination.dto';
 import { SkipAuth } from '../../../common/decorators/skip-auth.decorator';
