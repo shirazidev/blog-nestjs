8fcfeca8a738f7f956fbf5ea018c02ecdddc17df
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-06 12:26:08 +0330

    feat: enhance ImageService to include user ID in image creation

--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,16 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+  Scope,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ImageDto } from './dto/image.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ImageEntity } from './entities/image.entity';
 import { Repository } from 'typeorm';
 import { multerFile } from '../../common/utils/multer.util';
-@Injectable()
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
+import {
+  AuthMessage,
+  BadRequestMessage,
+  NotFoundMessage,
+  PublicMessage,
+} from '../../common/enums/message.enum';
+
+@Injectable({ scope: Scope.REQUEST })
 export class ImageService {
   constructor(
     @InjectRepository(ImageEntity)
     private imageRepository: Repository<ImageEntity>,
+    @Inject(REQUEST) private req: Request,
   ) {}
   create(createImageDto: ImageDto, image: multerFile) {
+  async create(createImageDto: ImageDto, image: multerFile) {
+    const userId = this.req?.user?.id;
+    const { alt, name } = createImageDto;
+    let location = image?.path?.slice(7);
+    await this.imageRepository.insert({
+      alt: alt || name,
+      name,
+      location,
+      userId,
+    });
+    return {
+      message: PublicMessage.Created,
+    };
+  }
+
     return image;
   }
 
