63121221c12a65e42b111883dac88a247b9e8fa4
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-06 11:48:57 +0330

    feat: add ImageController for managing image operations

---
+++ src/modules/image/image.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { ImageService } from './image.service';
+import { ImageDto } from './dto/image.dto';
+import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthDecorator } from '../../common/decorators/auth.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadFile } from '../../common/interceptors/upload.interceptor';
+import { multerFile } from '../../common/utils/multer.util';
+import { SwaggerConsumesEnum } from '../../common/enums/swagger-consumes.enum';
+
+@Controller('image')
+@ApiTags('images')
+@AuthDecorator()
+export class ImageController {
+  constructor(private readonly imageService: ImageService) {}
+
+  @Post()
+  @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
+  @UseInterceptors(UploadFile('image'))
+  create(@Body() createImageDto: ImageDto, @UploadedFile() image: multerFile) {
+    return this.imageService.create(createImageDto, image);
+  }
+
+  @Get()
+  @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
+  findAll() {
+    return this.imageService.findAll();
+  }
+
+  @Get(':id')
+  @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
+  findOne(@Param('id') id: string) {
+    return this.imageService.findOne(+id);
+  }
+
+  @Delete(':id')
+  @ApiConsumes(SwaggerConsumesEnum.JSON, SwaggerConsumesEnum.FORM)
+  remove(@Param('id') id: string) {
+    return this.imageService.remove(+id);
+  }
+}
