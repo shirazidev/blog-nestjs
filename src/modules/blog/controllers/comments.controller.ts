5b53d8c5682c540112968231f12c0759ad6972a8
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-06 11:52:06 +0330

    refactor: remove unused imports from comments.controller.ts

--- src/modules/blog/controllers/comments.controller.ts
+++ src/modules/blog/controllers/comments.controller.ts
@@ -7,10 +7,8 @@ import {
   Post,
   Put,
   Query,
-  UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '../../auth/guards/auth.guard';
-import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { CommentsService } from '../services/comments.service';
 import { SwaggerConsumesEnum } from '../../../common/enums/swagger-consumes.enum';
 import { CommentDto } from '../dtos/comment.dto';
