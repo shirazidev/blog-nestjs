65715aa488e1800178eb36ad5b7e2584d4cae222
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-11 01:20:05 +0330

    feat: import CustomHttpModule for enhanced HTTP request handling

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -9,20 +9,23 @@ import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
 import { CategoryModule } from '../category/category.module';
 import { BlogModule } from '../blog/blog.module';
+import { CustomHttpModule } from '../http/http.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: join(process.cwd(), '.env'),
-  }),
-  TypeOrmModule.forRootAsync({
-    useFactory: TypeOrmDbConfig
-  }),
-  AuthModule,
-  BlogModule,
-  CategoryModule,
-  UserModule,
-],
+  imports: [
+    CustomHttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), '.env'),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: TypeOrmDbConfig,
+    }),
+    AuthModule,
+    BlogModule,
+    CategoryModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
