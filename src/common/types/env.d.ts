f9398db9cfcc9dcf1fb9c35dee81491cf4a1abc3
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-10 21:28:24 +0330

    feat: add SEND_SMS_URL to ProcessEnv for SMS service configuration

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -1,21 +1,22 @@
 namespace NodeJS {
-    interface ProcessEnv {
-        // Application
-        PORT: number;
+  interface ProcessEnv {
+    // Application
+    PORT: number;
 
-        // Database
-        DBPORT: number;
-        DBHOST: string;
-        DBUSERNAME: string;
-        DBPASSWORD: string;
-        DB: string;
+    // Database
+    DBPORT: number;
+    DBHOST: string;
+    DBUSERNAME: string;
+    DBPASSWORD: string;
+    DB: string;
 
-        // Secrets
-        COOKIE_SECRET: string;
-        OTP_TOKEN_SECRET: string;
-        ACCESSTOKENJWT: string;
-        REFRESHTOKENJWT: string;
-        PHONE_SECRET_TOKEN: string;
-        EMAIL_SECRET_TOKEN: string;
-    }
+    // Secrets
+    COOKIE_SECRET: string;
+    OTP_TOKEN_SECRET: string;
+    ACCESSTOKENJWT: string;
+    REFRESHTOKENJWT: string;
+    PHONE_SECRET_TOKEN: string;
+    EMAIL_SECRET_TOKEN: string;
+    SEND_SMS_URL: string;
+  }
 }
