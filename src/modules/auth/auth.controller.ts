9aa4adc2c13352c217b1402c4d64e6a43d1f07ff
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-12 17:07:26 +0330

    feat: enhance auth controller with role-based access and decorators

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { AuthDto, CheckOtpDto } from './dto/auth.dto';
 import { SwaggerConsumesEnum } from 'src/common/enums/swagger-consumes.enum';
 import { Response, Request } from 'express';
 import { AuthGuard } from './guards/auth.guard';
-
+import { AuthDecorator } from '../../common/decorators/auth.decorator';
+import { CanAccess } from '../../common/decorators/role.decorator';
+import { Roles } from '../../common/enums/role.enum';
 
 @Controller('auth')
-@ApiTags("Auth")
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('/user-existence')
@@ -25,8 +38,8 @@ export class AuthController {
     // for production projects we use secure cookie names
   }
   @Get('/check-auth')
-  @ApiBearerAuth("Authorization")
-  @UseGuards(AuthGuard)
+  @AuthDecorator()
+  @CanAccess(Roles.Admin)
   async checkAuth(@Req() req: Request) {
     return req.user;
   }
