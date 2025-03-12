d70237536a3b94ae42f5b629a3aeb3cf0c02e340
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-12 17:07:49 +0330

    feat: implement RoleGuard for role-based access control

---
+++ src/modules/auth/guards/role.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { SKIP_AUTH } from '../../../common/decorators/skip-auth.decorator';
+import { ROLE_KEY } from '../../../common/decorators/role.decorator';
+import { Roles } from '../../../common/enums/role.enum';
+import { Request } from 'express';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  async canActivate(context: ExecutionContext) {
+    let requiredRoles = this.reflector.getAllAndOverride<Roles[]>(ROLE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles || requiredRoles.length == 0) return true;
+    if (!Array.isArray(requiredRoles)) {
+      requiredRoles = [requiredRoles];
+    }
+    const request: Request = context.switchToHttp().getRequest<Request>();
+    const user = request.user;
+    const userRole = user?.role;
+    if (user?.role === Roles.Admin) return true;
+    if (requiredRoles.includes(userRole as Roles)) return true;
+    throw new ForbiddenException();
+  }
+}
