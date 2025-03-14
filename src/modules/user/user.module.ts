0458ac34709c025a79ec68b789afd9a70f840f2a
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-14 11:26:12 +0330

    feat: add FollowEntity to user module imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -6,11 +6,17 @@ import { UserEntity } from './entities/user.entity';
 import { ProfileEntity } from './entities/profile.entity';
 import { OtpEntity } from './entities/otp.entity';
 import { AuthModule } from '../auth/auth.module';
+import { FollowEntity } from './entities/follow.entity';
 
 @Module({
   imports: [
     forwardRef(() => AuthModule),
-    TypeOrmModule.forFeature([UserEntity, ProfileEntity, OtpEntity]),
+    TypeOrmModule.forFeature([
+      UserEntity,
+      ProfileEntity,
+      OtpEntity,
+      FollowEntity,
+    ]),
   ],
   controllers: [UserController],
   providers: [UserService],
