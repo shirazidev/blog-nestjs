4306feb67a50c9105935e89ca65145528e638961
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-17 13:03:48 +0330

    feat: add GoogleUser type to response for Google authentication

--- src/modules/auth/types/response.ts
+++ src/modules/auth/types/response.ts
@@ -2,3 +2,11 @@ export type AuthResponse = {
   otpcode: string;
   token: string;
 };
+export type GoogleUser = {
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  picture?: string;
+  accessToken: string;
+  refreshToken: string;
+};
