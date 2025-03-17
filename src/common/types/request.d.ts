b2a85f01f5a404a1fb9e1450ae1c4130089e9964
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-17 03:58:35 +0330

    fix: extend Express Request interface to include user entity fix the bug on the user type in the request that caused too much errors

--- src/common/types/request.d.ts
+++ src/common/types/request.d.ts
@@ -1,9 +1,14 @@
-import { UserEntity } from "src/modules/user/entities/user.entity";
+import { UserEntity } from 'src/modules/user/entities/user.entity';
 
 declare global {
-    namespace Express {
-        interface Request {
-            user?: UserEntity;
-        }
+  namespace Express {
+    interface Request {
+      user?: UserEntity;
     }
+  }
+}
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: UserEntity;
+  }
 }
