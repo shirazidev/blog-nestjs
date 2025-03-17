014f446494bc3c10ce69a7e3333f8c992540a53b
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-17 13:05:05 +0330

    refactor: update CanAccess decorator to accept roles as metadata

--- src/common/decorators/role.decorator.ts
+++ src/common/decorators/role.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 import { Roles } from '../enums/role.enum';
 
 export const ROLE_KEY = 'ROLES';
-export const CanAccess = (...roles: Roles[]) => SetMetadata(ROLE_KEY, true);
+export const CanAccess = (...roles: Roles[]) => SetMetadata(ROLE_KEY, roles);
