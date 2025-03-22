fe63763edab9f63f24d41e0cbf72650b3a9a6073
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2025-03-22 15:30:10 +0330

    refactor: update TypeOrmDbConfig for consistent formatting and disable synchronization

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,18 +1,18 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export function TypeOrmDbConfig(): TypeOrmModuleOptions {
-        const {DB, DBHOST, DBUSERNAME, DBPORT, DBPASSWORD} = process.env;
-        return {
-            type: "postgres",
-            database: DB,
-            port: DBPORT,
-            host: DBHOST,
-            username: DBUSERNAME,
-            password: DBPASSWORD,
-            synchronize: true,
-            entities: [
-                "dist/**/**/**/*.entity{.ts,.js}",
-                "dist/**/**/*.entity{.ts,.js}",
-            ]
-        }
-    }
+  const { DB, DBHOST, DBUSERNAME, DBPORT, DBPASSWORD } = process.env;
+  return {
+    type: 'postgres',
+    database: DB,
+    port: DBPORT,
+    host: DBHOST,
+    username: DBUSERNAME,
+    password: DBPASSWORD,
+    synchronize: false,
+    entities: [
+      'dist/**/**/**/*.entity{.ts,.js}',
+      'dist/**/**/*.entity{.ts,.js}',
+    ],
+  };
+}
